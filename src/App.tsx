3b52521731dd26ca6f1f470cba57c002f100f382
wlphp <613154514@qq.com>
2026-09-18 13:32:22 +0800

    Release v0.1.30

--- src/App.tsx
+++ src/App.tsx
@@ -176,7 +176,7 @@ function syncAssetTypes(account: Account): ReadonlyArray<(typeof assetTypes)[num
 }
 const assetTypes = catalogAssetTypes;
 
-const bundledVersion = "0.1.29";
+const bundledVersion = "0.1.30";
 const isDevelopmentBuild = import.meta.env.DEV;
 
 const detachedTerminalHostId = (() => {
